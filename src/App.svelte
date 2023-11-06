afc4ab9a0b25fe1b22f77cd206eedab9de46c60f
YKESHTFR <ykshtfr@gmail.com>
2023-11-06 17:29:30 -0500

    Pagina inicial, filas, columnas

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,91 @@
 <script>
-
 </script>
 
-<main>
-	<section>
-		<h1>Metodos Numericos Aplicativos</h1>
-	</section>
-	<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+<main class="home">
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Metodos Númericos Aplicativos</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon" />
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
-<button type="button" class="btn btn-link">Link</button>
+	<div class="container text-center">
+		<div class="row">
+			<div class="col-4">
+				Parte de seleccion
+				<div class="card" style="width: auto;">
+					<div class="card-body">
+						<h5 class="card-title">Método de Bisección</h5>
+						<a href="#" class="btn btn-primary" style="color: white;">Vamos!</a>
+					</div>
+				</div>
+
+				<div class="card" style="width: auto;">
+					<div class="card-body">
+						<h5 class="card-title">Método de Bisección</h5>
+						<a href="#" class="btn btn-primary" style="color: white;">Vamos!</a>
+					</div>
+				</div>
+				
+				<br>
+				
+				<div class="card" style="width: auto;">
+					<div class="card-body">
+						<h5 class="card-title">Ingrese los datos</h5>
+						<br>
+						<form>
+							<div class="mb-3">
+								<label for="exampleInputNcs" class="form-label">No. cifras significativas</label>
+								<input type="email" class="form-control" id="exampleInputNcs" aria-describedby="emailHelp">
+								<div id="ncsHelp" class="form-text">Ingrese el número de cifras significativas para su cálculo</div>
+							</div>
+							<div class="mb-3">
+								<label for="exampleInputXi" class="form-label">Xi</label>
+								<input type="email" class="form-control" id="exampleInputXi" aria-describedby="emailHelp">
+								<div id="xiHelp" class="form-text">Ingrese el limite superior</div>
+							</div>
+							<div class="mb-3">
+								<label for="exampleInputXs" class="form-label">Xs</label>
+								<input type="email" class="form-control" id="exampleInputXs" aria-describedby="emailHelp">
+								<div id="xsHelp" class="form-text">Ingrese el limite inferior</div>
+							</div>
+							
+							<button type="submit" class="btn btn-primary">Calcular</button>
+						</form>
+					</div>
+				</div>
+
+			</div>
+			<div class="col">Parte de visualizacion
+				<div class="card" style="width: 100%; height: 100vh;">
+					<div class="card-body">
+						<h5 class="card-title">Resutado de la operación</h5>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
